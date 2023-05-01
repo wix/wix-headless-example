4dc2906f164c9bb81e6f60ac74081f5ba6d1b23a
Shahar Talmi <shahar@wix.com>
2023-05-01 19:25:07 +0300

    added tickets

--- pages/tickets.js
+++ pages/tickets.js
@@ -4,17 +4,54 @@ import { Inter } from 'next/font/google'
 import { useEffect, useState } from 'react';
 
 import { createClient, OAuthStrategy } from '@wix/api-client';
-import { wixEvents } from '@wix/events';
+import { wixEvents, checkout } from '@wix/events';
 import { redirects } from '@wix/redirects';
 
 const inter = Inter({ subsets: ['latin'] })
 
 const myWixClient = createClient({
-  modules: { wixEvents, redirects },
+  modules: { wixEvents, checkout, redirects },
   auth: OAuthStrategy({ clientId: `10c1663b-2cdf-47c5-a3ef-30c2e8543849` })
 });
 
-export default function Home() {
+export default function Tickets() {
+  const [eventsList, setEventsList] = useState([]);
+  const [ticketsAvailability, setTicketsAvailability] = useState([]);
+
+  async function fetchEvents() {
+    const eventsList = await myWixClient.wixEvents.queryEventsV2({ query: { paging: { limit: 10 } } });
+    console.log(eventsList)
+    setEventsList(eventsList.events);
+  }
+
+  async function fetchTicketsAvailability(event) {
+    const tickets = await myWixClient.checkout.queryAvailableTickets({
+      filter: { eventId: event._id },
+      offset: 0,
+      limit: 100,
+      sort: 'orderIndex:asc',
+    });
+    console.log(tickets);
+    setTicketsAvailability(tickets.definitions);
+  }
+
+  async function createRedirect(ticket) {
+    debugger;
+    const eventSlug = eventsList.find((event) => event._id === ticket.eventId).slug;
+    const reservation = await myWixClient.checkout.createReservation(ticket.eventId, {
+      ticketQuantities: [{
+        ticketDefinitionId: ticket._id,
+        quantity: 1,
+      }],
+    });
+    const redirect = await myWixClient.redirects.createRedirectSession({
+      eventsCheckout: { eventSlug, reservationId: reservation._id }
+    });
+    window.location = redirect.redirectSession.fullUrl;
+  }
+
+  useEffect(() => { fetchEvents(); }, []);
+
   return (
     <>
       <Head>
@@ -24,7 +61,18 @@ export default function Home() {
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.grid}>
-          Hello world!
+          <div>
+            <h2>Choose Event:</h2>
+            {eventsList.map((event) => {
+              return <div className={styles.card} key={event._id} onClick={() => fetchTicketsAvailability(event)}>{event.title}</div>;
+            })}
+          </div>
+          <div>
+            <h2>Choose Tickets:</h2>
+            {ticketsAvailability.map((ticket) => {
+              return <div className={styles.card} key={ticket._id} onClick={() => createRedirect(ticket)}>{ticket.name}</div>;
+            })}
+          </div>
         </div>
       </main>
     </>
