335f90b84dbcbdc0667d172f21938f1a2c70b6d5
Ronen Sivak <140695222+RonenSivak@users.noreply.github.com>
2024-05-19 14:27:02 +0300

    ran prettier

--- src/utils/test-ids.js
+++ src/utils/test-ids.js
@@ -1,22 +1,22 @@
 const ids = {
   BOOKINGS_PAGE: {
-    CONTAINER: 'bookings.container',
-    SERVICE: 'bookings.service',
-    SLOT: 'bookings.slot',
+    CONTAINER: "bookings.container",
+    SERVICE: "bookings.service",
+    SLOT: "bookings.slot",
   },
   COMMERCE_PAGE: {
-    CONTAINER: 'commerce.container',
-    PRODUCT: 'product.container',
-    CHECKOUT: 'product.checkout.cta',
+    CONTAINER: "commerce.container",
+    PRODUCT: "product.container",
+    CHECKOUT: "product.checkout.cta",
   },
   EVENTS_PAGE: {
-    CONTAINER: 'events.container',
-    EVENT: 'events.event',
-    TICKET_OPTION: 'events.ticket-option',
+    CONTAINER: "events.container",
+    EVENT: "events.event",
+    TICKET_OPTION: "events.ticket-option",
   },
   SUBSCRIPTIONS_PAGE: {
-    CONTAINER: 'subscriptions.container',
-    PRICING_PLAN: 'subscriptions.pricing-plan',
+    CONTAINER: "subscriptions.container",
+    PRICING_PLAN: "subscriptions.pricing-plan",
   },
 };
 
