12f351529a7b1aeab748bc74c1c49aa7cdf22c3f
Ronen Sivak <140695222+RonenSivak@users.noreply.github.com>
2024-08-29 15:47:40 +0300

    fixed cors error when trying to validate users redirect uri

--- src/utils/validate-oauth-url.js
+++ src/utils/validate-oauth-url.js
@@ -1,23 +1,18 @@
 export const isValidOauthUrl = async (myWixClient) => {
-  try {
-    const data = myWixClient.auth.generateOAuthData(
-      `${window.location.origin}/login-callback`,
-      window.location.href,
-    );
-    const { authUrl } = await myWixClient.auth.getAuthUrl(data);
+    try {
+        const data = myWixClient.auth.generateOAuthData(
+            `${window.location.origin}/login-callback`,
+            window.location.href,
+        );
+        const {authUrl} = await myWixClient.auth.getAuthUrl(data);
+        const corsProxy = 'https://thingproxy.freeboard.io/fetch/'; // CORS proxy to bypass CORS restrictions
+        const url = corsProxy + encodeURIComponent(authUrl);
+        // Attempt to fetch the URL to check if it's valid and accessible
+        const response = await fetch(url);
 
-    // Attempt to fetch the authUrl and check if it's reachable
-    const response = await fetch(authUrl, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    // Check if the response was successful
-    return response.ok;
-  } catch (error) {
-    console.error("Error checking authUrl:", error);
-    return false;
-  }
+        // Check if the response was successful
+        return response.status === 200;
+    } catch (error) {
+        return false;
+    }
 };
