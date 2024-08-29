3deb316ac7c9a71fc3fb84dfbcf57175b34ab632
Ronen Sivak <140695222+RonenSivak@users.noreply.github.com>
2024-08-29 10:49:48 +0300

    lint

--- src/components/ui/modal.js
+++ src/components/ui/modal.js
@@ -3,61 +3,56 @@
 import styles from "@/styles/modal.module.css";
 
 export default function Modal({
-                                  openModal,
-                                  title,
-                                  content,
-                                  primaryText,
-                                  secondaryText,
-                                  primaryAction,
-                                  secondaryAction
-                              }) {
-
-    return (
-        <>
-            {openModal && (
-                <div className={styles.modalOverlay}>
-                    <div className={styles.modal}>
-                        <div className={styles.modalBody}>
-                            <div className={styles.modalContent}>
-                                <svg
-                                    className={styles.icon}
-                                    fill="none"
-                                    stroke="currentColor"
-                                    viewBox="0 0 24 24"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth="2"
-                                        d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                                    />
-                                </svg>
-                                <h3 className={styles.modalTitle}>
-                                    {title}
-                                </h3>
-                                <p className={styles.modalText}>
-                                    {content}
-                                </p>
-                                <div className={styles.buttonContainer}>
-                                    <button
-                                        className={`${styles.button} ${styles.buttonPrimary}`}
-                                        onClick={primaryAction}
-                                    >
-                                        {primaryText}
-                                    </button>
-                                    <button
-                                        className={`${styles.button} ${styles.buttonSecondary}`}
-                                        onClick={secondaryAction}
-                                    >
-                                        {secondaryText}
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+  openModal,
+  title,
+  content,
+  primaryText,
+  secondaryText,
+  primaryAction,
+  secondaryAction,
+}) {
+  return (
+    <>
+      {openModal && (
+        <div className={styles.modalOverlay}>
+          <div className={styles.modal}>
+            <div className={styles.modalBody}>
+              <div className={styles.modalContent}>
+                <svg
+                  className={styles.icon}
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+                <h3 className={styles.modalTitle}>{title}</h3>
+                <p className={styles.modalText}>{content}</p>
+                <div className={styles.buttonContainer}>
+                  <button
+                    className={`${styles.button} ${styles.buttonPrimary}`}
+                    onClick={primaryAction}
+                  >
+                    {primaryText}
+                  </button>
+                  <button
+                    className={`${styles.button} ${styles.buttonSecondary}`}
+                    onClick={secondaryAction}
+                  >
+                    {secondaryText}
+                  </button>
                 </div>
-            )}
-        </>
-    );
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
