f64b968ac3634bbcdb99a731a2664f2fe83a8828
Ronen Sivak <140695222+RonenSivak@users.noreply.github.com>
2024-09-04 11:30:13 +0300

    content change

--- pages/members.js
+++ pages/members.js
@@ -32,8 +32,6 @@ const myWixClient = createClient({
   }),
 });
 
-const WixLogin = () => {};
-
 export default function LoginBar() {
   // State variable to store the current member.
   const [member, setMember] = useState(null);
@@ -211,7 +209,7 @@ export default function LoginBar() {
                   <path d="M384 336l-192 0c-8.8 0-16-7.2-16-16l0-256c0-8.8 7.2-16 16-16l140.1 0L400 115.9 400 320c0 8.8-7.2 16-16 16zM192 384l192 0c35.3 0 64-28.7 64-64l0-204.1c0-12.7-5.1-24.9-14.1-33.9L366.1 14.1c-9-9-21.2-14.1-33.9-14.1L192 0c-35.3 0-64 28.7-64 64l0 256c0 35.3 28.7 64 64 64zM64 128c-35.3 0-64 28.7-64 64L0 448c0 35.3 28.7 64 64 64l192 0c35.3 0 64-28.7 64-64l0-32-48 0 0 32c0 8.8-7.2 16-16 16L64 464c-8.8 0-16-7.2-16-16l0-256c0-8.8 7.2-16 16-16l32 0 0-48-32 0z" />
                 </svg>
               </span>
-              ) to the Wix App settings.
+              ) to the OAuth App settings.
               <Link
                 href={`https://manage.wix.com/dashboard/${msid}/oauth-apps-settings/manage/${CLIENT_ID}`}
                 target={"_blank"}
